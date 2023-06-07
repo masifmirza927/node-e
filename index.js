fe863f02448cfbb1df645476fd7fa0693d12cf79
masifmirza927 <masifmirza927@gmail.com>
2023-06-07 17:09:44 +0500

    fs mofule

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-const os = require("os");
+// const os = require("os");
+const fs = require("fs");
 
 // const {sum, multiply} = require("./functions");
 // sum(3, 8);
@@ -8,3 +9,30 @@ const os = require("os");
 // console.log( os.platform() );
 // console.log( os.cpus().length );
 // console.log(os.uptime())
+
+
+
+
+//  file system module fs
+    // creating new file
+    // fs.writeFile("sample.txt", "this is my first file", (error) => {
+    //     if(error) {
+    //         console.log(error)
+    //     }else {
+    //         console.log("successfully created");
+    //     }
+    // })
+
+// reading files
+    // const content =  fs.readFileSync("./sample.txt", "utf-8");
+    // console.log(content)
+
+// deleting file    
+//    fs.unlink("./sample.txt", () => {
+//     console.log("file deleted");
+//    });
+
+// creating directory/folder
+fs.mkdir("./files", () => {
+    console.log("folder created");
+});
